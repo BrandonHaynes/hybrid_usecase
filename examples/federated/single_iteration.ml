10e9bca988fcc39266c8cccdc5e25dd4ab7200ac
Brandon Haynes <bhaynes@cs.washington.edu>
2015-07-23 12:25:00 -0700

    Removed alpha from example

--- examples/federated/single_iteration.ml
+++ examples/federated/single_iteration.ml
@@ -1,10 +1,7 @@
-const bins: 10;
-vectors = scan(vectors);
-
 -------------------------
 -- Constants + Functions
 -------------------------
-const alpha: 1.0;
+const bins: 10;
 
 def iif(expression, true_value, false_value):
     case when expression then true_value
@@ -15,20 +12,21 @@ def difference(current, previous, previous_time, time):
     iif(previous_time >= 0,
         (current - previous) * iif(previous_time < time, 1, -1),
         current);
+uda HarrTransformGroupBy(time, x) {
+  [0.0 as coefficient, 0.0 as _sum, 0 as _count, -1 as _time];
+  [difference(x, coefficient, _time, time), _sum + x, _count + 1, time];
+  [coefficient, _sum / int(_count)];
+};
 
 ------------------------------------------------------------------------------------
 -- Harr Transform
 ------------------------------------------------------------------------------------
-uda HarrTransformGroupBy(alpha, time, x) {
-  [0.0 as coefficient, 0.0 as _sum, 0 as _count, -1 as _time];
-  [difference(x, coefficient, _time, time), _sum + x, _count + 1, time];
-  [coefficient, _sum / int(_count * alpha)];
-};
+vectors = scan(vectors);
 
 groups = [from vectors emit
                  id,
                  int(floor(time/2)) as time,
-                 HarrTransformGroupBy(alpha, time, value) as [coefficient, mean]];
+                 HarrTransformGroupBy(time, value) as [coefficient, mean]];
 
 coefficients = [from groups emit id, coefficient];
 range = [from vectors emit max(value) - min(value) as high, min(value) - max(value) as low];

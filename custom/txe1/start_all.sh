1af5467967e4c7b3d21342fcbe4a8967a7fe9d48
Brandon Haynes <bhaynes@cs.washington.edu>
2015-07-31 14:03:10 -0400

    Start-all tweaks

--- custom/txe1/start_all.sh
+++ custom/txe1/start_all.sh
@@ -5,6 +5,9 @@ cd scidb
 ./scidb_cluster.sh start "$@"
 #. myria/myria_cluster.sh start "$@" --config scidb/myria.conf
 
+echo ------------------------------------------------------
+echo Initializing SciDB with arrays and custom aggregates
+echo ------------------------------------------------------
 ssh $1 "$SCIDB_BASE/bin/iquery -p $SCIDB_BASE_PORT -anq 'store(build(<value: double>[id=0:599,1,0, time=0:255,256,0], (double(id+1) / (time+1)) / 600 - 1), SciDB__Demo__Vectors)'"
 ssh $1 "$SCIDB_BASE/bin/iquery -p $SCIDB_BASE_PORT -anq \"load_library('bin')\""
 #ssh node-038 "/state/partition1/scidb-bhaynes/bin/iquery -p $SCIDB_BASE_PORT -anq 'store(build(<value: double>[id=0:599,1,0, time=0:255,256,0], (double(id+1) / (time+1)) / 600 - 1), SciDB__Demo__Vectors)'"
@@ -12,12 +15,12 @@ ssh $1 "$SCIDB_BASE/bin/iquery -p $SCIDB_BASE_PORT -anq \"load_library('bin')\""
 echo ------------------------------------------------------
 echo Starting demo screen webserver
 echo ------------------------------------------------------
-ssh $1 nohup python ~/hybrid_usecase/website/server_txe1.py --scidb-path $SCIDB_BASE
+ssh $1 nohup python ~/hybrid_usecase/website/server_txe1.py --scidb-path $SCIDB_BASE &
 
 echo ------------------------------------------------------
 echo Ensuring Myria-Web webserver
 echo ------------------------------------------------------
-ssh $1 nohup /state/partition1/myria_bhaynes/stack/google_appengine/dev_appserver.py --host node-038 --port 8090 --admin_port 8091 --datastore_path /state/partition1/myria_bhaynes/stack/myria-web/database --logs_path /state/partition1/myria_bhaynes/stack/myria-web/logs --skip_sdk_update_check true /state/partition1/myria_bhaynes/stack/myria-web/appengine
+ssh $1 nohup /state/partition1/myria_bhaynes/stack/google_appengine/dev_appserver.py --host node-038 --port 8090 --admin_port 8091 --datastore_path /state/partition1/myria_bhaynes/stack/myria-web/database --logs_path /state/partition1/myria_bhaynes/stack/myria-web/logs --skip_sdk_update_check true /state/partition1/myria_bhaynes/stack/myria-web/appengine &
 
 echo ------------------------------------------------------
 echo Required SSH tunnels:

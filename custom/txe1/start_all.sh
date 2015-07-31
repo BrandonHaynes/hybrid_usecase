78d1a69837e119660ebac05059b4341668b1db08
Brandon Haynes <bhaynes@cs.washington.edu>
2015-07-31 16:28:29 -0400

    Debugging

--- custom/txe1/start_all.sh
+++ custom/txe1/start_all.sh
@@ -46,6 +46,13 @@ echo ------------------------------------------------------
 ssh $1 "nohup /state/partition1/myria_bhaynes/stack/google_appengine/dev_appserver.py --host node-038 --port 8090 --admin_port 8091 --datastore_path /state/partition1/myria_bhaynes/stack/myria-web/database --logs_path /state/partition1/myria_bhaynes/stack/myria-web/logs --skip_sdk_update_check true /state/partition1/myria_bhaynes/stack/myria-web/appengine" &
 
 echo ------------------------------------------------------
+echo Ensuring Myria Relations
+echo ------------------------------------------------------
+echo SciDB:Demo:Vectors
+curl -trace -vi -H "Content-Type: application/x-www-form-urlencoded" -X POST -d 'query=singleton_symbols+%3D+%5B1+as+id%2C+0+as+index%2C+0.5+as+value%5D%3B+shuffled_symbols+%3D+%5Bfrom+singleton_symbols+emit+id%2C+index%2C+value%2C+count%28%2A%29%5D%3B+symbols+%3D+%5Bfrom+shuffled_symbols+emit+id%2C+index%2C+value%5D%3B+store%28symbols%2C+public%3Aadhoc%3Asymbols%29%3B&language=myrial' http://node-038:8090/execute
+
+
+echo ------------------------------------------------------
 echo Required SSH tunnels:
 echo ------------------------------------------------------
 echo ssh txe1-login.mit.edu -L 8080:172.16.4.60:8080 -N -f

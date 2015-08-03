e1ce0e576ae7a0c6fcd783579d8a98d8ca1c66b0
Brandon Haynes <bhaynes@cs.washington.edu>
2015-08-03 10:33:45 -0400

    Startup modifications

--- custom/txe1/start_all.sh
+++ custom/txe1/start_all.sh
@@ -5,6 +5,7 @@ source myria/myria.conf
 
 COORDINATOR=$1
 WORKERS=$@
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd scidb
 ./scidb_cluster.sh start "$@"
@@ -42,12 +43,12 @@ done
 echo ------------------------------------------------------
 echo Starting demo screen webserver
 echo ------------------------------------------------------
-ssh $COORDINATOR "nohup python ~/hybrid_usecase/website/server_txe1.py --scidb-path $SCIDB_BASE" &
+ssh $COORDINATOR "nohup python $BASEDIR/../../website/server_txe1.py --scidb-path $SCIDB_BASE &"
 
 echo ------------------------------------------------------
 echo Ensuring Myria-Web webserver
 echo ------------------------------------------------------
-ssh $COORDINATOR "nohup /state/partition1/myria_bhaynes/stack/google_appengine/dev_appserver.py --host node-038 --port 8090 --admin_port 8091 --datastore_path /state/partition1/myria_bhaynes/stack/myria-web/database --logs_path /state/partition1/myria_bhaynes/stack/myria-web/logs --skip_sdk_update_check true /state/partition1/myria_bhaynes/stack/myria-web/appengine" &
+ssh $COORDINATOR "nohup $MYRIA_BASE/stack/google_appengine/dev_appserver.py --host node-038 --port 8090 --admin_port 8091 --datastore_path /state/partition1/myria_bhaynes/stack/myria-web/database --logs_path /state/partition1/myria_bhaynes/stack/myria-web/logs --skip_sdk_update_check true /state/partition1/myria_bhaynes/stack/myria-web/appengine &"
 
 echo ------------------------------------------------------
 echo Ensuring Myria Relations
@@ -55,13 +56,11 @@ echo ------------------------------------------------------
 echo SciDB:Demo:Vectors
 curl -trace -vi -H "Content-Type: application/x-www-form-urlencoded" -X POST -d 'query=singleton_symbols+%3D+%5B1+as+id%2C+0+as+index%2C+0.5+as+value%5D%3B+shuffled_symbols+%3D+%5Bfrom+singleton_symbols+emit+id%2C+index%2C+value%2C+count%28%2A%29%5D%3B+symbols+%3D+%5Bfrom+shuffled_symbols+emit+id%2C+index%2C+value%5D%3B+store%28symbols%2C+public%3Aadhoc%3Asymbols%29%3B&language=myrial' http://node-038:8090/execute
 
-cd myria
 MYRIA_REST_PORT=${MYRIA_REST_PORT=8753}
 MYRIA_HTTP_PORT=${MYRIA_HTTP_PORT=8090}
-BASEDIR=$(dirname $0)
-ssh $COORDINATOR "cd $BASEDIR/myria && ./create_vectors.py 600 256 --url http://$COORDINATOR:$MYRIA_REST_PORT \
-                                                                   --execution-url http://$COORDINATOR:$MYRIA_HTTP_PORT \
-                                                                   --relation-name vectors"
+ssh $COORDINATOR "cd $BASEDIR/myria && python create_vectors.py 600 256 --url http://$COORDINATOR:$MYRIA_REST_PORT \
+                                                                        --execution-url http://$COORDINATOR:$MYRIA_HTTP_PORT \
+                                                                        --relation-name vectors"
 
 echo ------------------------------------------------------
 echo Required SSH tunnels:

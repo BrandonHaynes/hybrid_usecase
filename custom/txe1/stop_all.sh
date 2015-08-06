a91438822b0e27bee893ae66e30e172612b1caf0
Brandon Haynes <bhaynes@cs.washington.edu>
2015-08-06 12:11:17 -0700

    Stop Myria too...

--- custom/txe1/stop_all.sh
+++ custom/txe1/stop_all.sh
@@ -5,7 +5,7 @@ source myria/myria.conf
 
 cd scidb
 ./scidb_cluster.sh stop "$@"
-#cd ../myria
-#./myria_cluster.sh stop "$@"
+cd ../myria
+./myria_cluster.sh stop "$@"
 
 ssh $1 "pkill -f server_txe1.py"

b230b2afeb64925518ee72b647d77c807b207db5
Brandon Haynes <bhaynes@cs.washington.edu>
2015-08-11 09:47:25 -0700

    Website tweaks

--- website/js/screen.js
+++ website/js/screen.js
@@ -78,76 +78,89 @@ function populateResults(ids) {
     var test_id = +$('tr.info td:first').text();
     var test_index = $('tr.info').parent().children().index($('tr.info'));
 
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8751/dataset?public:adhoc:correlations', //TODO
+        contentType: 'application/json',
+        dataType: 'json',
+
+        success: function(data) {
+            var pairs = data.sort(function(a, b) { return b['rho'] - a['rho']});
+            var validPairs = pairs.filter(function(pair) { return pair['id'] <  $('#patients tbody tr').length });
+            var indexes = [test_id].concat(validPairs.slice(0, 5).map(function(pair) { return pair['id']; }));
+            var ids = indexes.map(function(index) { return $('td:first', $('#patients tbody tr')[index]).text(); });
+
+            // Yay!  SQL injection!
+            execute("RELATION(select subject_id, dob, sex from mimic2v26.d_patients where subject_id in (" + ids.join(',') + ") order by subject_id)", function(data) {
+                data.tuples = data.tuples.sort(function(a, b) { return ids.indexOf(a[0]) - ids.indexOf(b[0]) });
+
+                var rows = d3.select("#results tbody")
+                             .selectAll("tr")
+                             .data(data.tuples, function(d) { return d; });
+                rows.enter().append("tr");
+                rows.exit().remove();
+                rows.selectAll("td")
+                    .data(function(row, i) {
+                            return data.schema.map(function(name, index) {
+                                    return {name: name, value: row[index]};
+                                }).concat({'name': 'rho', 'value': (i == 0 ? 1 : validPairs[i]['rho']).toString() }, {'name': 'waveform', 'value': getWaveform(row)}, {'id': +row[0], 'name': 'status', 'value': is_stable(row[0]) ? 'Stable' : 'Unstable'});
+                            })
+                    .enter()
+                    .append("td")
+                    .attr('class', function(d) { return d.name == 'status' ? (is_stable(d.id) ? 'success' : 'danger') : '' })
+                    .html(function(d) {
+                        return d.name != 'waveform' ? d.value.replace(" 00:00:00.0", "") : populateWaveform.call(this, d); });
+
+                $('#similar-patients').fadeIn();
+
+                populatePrediction(rows[0]);
+            }, function(data) {
+                populateResultsError(test_id);
+            });
+        },
+        error: function(d) {
+            populateResultsError(test_id);
+        }
+      });
+
 	//var ids = [124, 177, 283, 308, 377, 408, 491, 521, 565, 593, 618, 631, 634, 668, 682, 719, 787, 871, 985, 992, 1092, 1158, 1182, 1202, 1207, 1313, 1357, 1378, 1449, 1453, 1474, 1501, 1521, 1528, 1531, 1586, 1604, 1606, 1621, 1758, 1795, 1855, 1868, 1892, 1898, 1931, 1944, 1950, 1979, 2014, 2187, 2213, 2228, 2261, 2332, 2395, 2477, 2488, 2561, 2614, 2664, 2708, 2725, 2827, 2906, 3024, 3165, 3245, 3261, 3286, 3287, 3372, 3386, 3424, 3466, 3474, 3554, 3593, 3652, 3759, 3780, 3830, 3883, 3884, 3886, 3929, 3932, 4068, 4076, 4077, 4252, 4254, 4263, 4264, 4270, 4338, 4348, 4350, 4369, 4474, 4565, 4655, 4806, 4833, 4893, 4894, 4968, 5198, 5201, 5205, 5254, 5282, 5307, 5354, 5369, 5382, 5494, 5506, 5569, 5606, 5607, 5619, 5632, 5675, 5701, 5712, 5738, 5742, 5784, 5786, 5791, 5896, 5957, 5960, 5964, 6017, 6039, 6042, 6053, 6077, 6335, 6349, 6464, 6470, 6605, 6707, 6809, 6892, 6983, 7224, 7225, 7234, 7251, 7265, 7328, 7432, 7492, 7497, 7519, 7522, 7528, 7655, 7728, 7760, 7782, 7786, 7860, 7874, 7910, 7985, 8009, 8084, 8087, 8115, 8126, 8141, 8186, 8187, 8221, 8231, 8368, 8396, 8445, 8509, 8516, 8569, 8718, 8734, 8749, 8896, 8915, 8985, 8996, 9031, 9130, 9258, 9278, 9300, 9335, 9338, 9358, 9473, 9526, 9536, 9595, 9642, 9732, 9920, 9951, 10045, 10083, 10188, 10241, 10277, 10305, 10315, 10362, 10487, 10525, 10534, 10552, 10564, 10653, 10842, 10872, 10906, 10925, 11004, 11086, 11137, 11191, 11244, 11280, 11403, 11590, 11609, 11684, 11698, 11710, 11763, 11764, 11850, 12090, 12212, 12215, 12231, 12331, 12351, 12372, 12461, 12573, 12581, 12632, 12704, 12727, 12739, 12807, 12821, 12915, 12974, 13033, 13136, 13274, 13355, 13438, 13536, 13552, 13599, 13600, 13640, 13646, 13715, 13720, 13728, 13759, 13852, 13868, 13993, 14059, 14123, 14168, 14266, 14291, 14410, 14486, 14561, 14579, 14622, 14626, 14669, 14763, 14784, 14855, 14935, 14936, 15110, 15143, 15208, 15218, 15266, 15279, 15329, 15330, 15332, 15382, 15457, 15470, 15509, 15524, 15624, 15646, 15654, 15703, 15779, 15809, 15902, 15903, 15911, 15929];
+    //var ids = [124, 177, 283, 308, 377, 408, 491, 521, 565, 593, 618, 631, 634, 668, 682, 719, 787, 871, 985, 992, 1092, 1158, 1182, 1202, 1207, 1313, 1357, 1378, 1449, 1453, 1474, 1501, 1521, 1528, 1531, 1586, 1604, 1606, 1621, 1758, 1795, 1855, 1868, 1892, 1898, 1931, 1944, 1950, 1979, 2014, 2187, 2213, 2228, 2261, 2332, 2395, 2477, 2488, 2561, 2614, 2664, 2708, 2725, 2827, 2906, 3024, 3165, 3245, 3261, 3286, 3287, 3372, 3386, 3424, 3466, 3474, 3554, 3593, 3652, 3759, 3780, 3830, 3883, 3884, 3886, 3929, 3932, 4068, 4076, 4077, 4252, 4254, 4263, 4264, 4270, 4338, 4348, 4350, 4369, 4474, 4565, 4655, 4806, 4833, 4893, 4894, 4968, 5198, 5201, 5205, 5254, 5282, 5307];
+}
+
+function populateResultsError(test_id) {
     var ids = [124, 177, 283, 308, 377, 408, 491, 521, 565, 593, 618, 631, 634, 668, 682, 719, 787, 871, 985, 992, 1092, 1158, 1182, 1202, 1207, 1313, 1357, 1378, 1449, 1453, 1474, 1501, 1521, 1528, 1531, 1586, 1604, 1606, 1621, 1758, 1795, 1855, 1868, 1892, 1898, 1931, 1944, 1950, 1979, 2014, 2187, 2213, 2228, 2261, 2332, 2395, 2477, 2488, 2561, 2614, 2664, 2708, 2725, 2827, 2906, 3024, 3165, 3245, 3261, 3286, 3287, 3372, 3386, 3424, 3466, 3474, 3554, 3593, 3652, 3759, 3780, 3830, 3883, 3884, 3886, 3929, 3932, 4068, 4076, 4077, 4252, 4254, 4263, 4264, 4270, 4338, 4348, 4350, 4369, 4474, 4565, 4655, 4806, 4833, 4893, 4894, 4968, 5198, 5201, 5205, 5254, 5282, 5307];
-	// Yay!  SQL injection!
-	execute("RELATION(select subject_id, dob, sex from mimic2v26.d_patients where subject_id in (" + ids.join(',') + ") order by subject_id limit 5)", function(data) {
-        //TODO remove failsafe?
-        var lookup = function(id) {
-            row = $('td', $('#patients tbody tr')[ids.indexOf(id)]);
-            return [row[0].innerText, row[1].innerText, row[2].innerText, row[3].innerText]; };
-        data.schema = ['subject_id', 'dob', 'sex'];
-        data.tuples = [lookup(test_id)];
-        data.tuples.push(lookup(ids[+data.tuples[0][0] % ids.length]));
-        data.tuples.push(lookup(ids[+data.tuples[1][0] % ids.length]));
-        data.tuples.push(lookup(ids[+data.tuples[2][0] % ids.length]));
-        data.tuples.push(lookup(ids[+data.tuples[3][0] % ids.length]));
-        data.tuples.push(lookup(ids[+data.tuples[4][0] % ids.length]));
 
-	    var rows = d3.select("#results tbody")
-                     .selectAll("tr")
-	                 .data(data.tuples, function(d) { return d; });
-        rows.enter().append("tr");
-        rows.exit().remove();
-		rows.selectAll("td")
-		    .data(function(row, i) {
-		            return data.schema.map(function(name, index) {
-		                	return {name: name, value: row[index]};
-			            }).concat({'name': 'rho', 'value': map_rho(test_id, row[0], i)}, {'name': 'waveform', 'value': getWaveform(row)}, {'id': +row[0], 'name': 'status', 'value': is_stable(row[0]) ? 'Stable' : 'Unstable'});
-			        })
-		    .enter()
-		    .append("td")
-		    .attr('class', function(d) { return d.name == 'status' ? (is_stable(d.id) ? 'success' : 'danger') : '' })
-	        .html(function(d) {
-                return d.name != 'waveform' ? d.value.replace(" 00:00:00.0", "") : populateWaveform.call(this, d); });
+    var lookup = function(id) {
+        row = $('td', $('#patients tbody tr')[ids.indexOf(id)]);
+        return [row[0].innerText, row[1].innerText, row[2].innerText, row[3].innerText]; };
+    data.schema = ['subject_id', 'dob', 'sex'];
+    data.tuples = [lookup(test_id)];
+    data.tuples.push(lookup(ids[+data.tuples[0][0] % ids.length]));
+    data.tuples.push(lookup(ids[+data.tuples[1][0] % ids.length]));
+    data.tuples.push(lookup(ids[+data.tuples[2][0] % ids.length]));
+    data.tuples.push(lookup(ids[+data.tuples[3][0] % ids.length]));
+    data.tuples.push(lookup(ids[+data.tuples[4][0] % ids.length]));
 
-	    $('#similar-patients').fadeIn();
+    var rows = d3.select("#results tbody")
+                 .selectAll("tr")
+                 .data(data.tuples, function(d) { return d; });
+    rows.enter().append("tr");
+    rows.exit().remove();
+    rows.selectAll("td")
+        .data(function(row, i) {
+                return data.schema.map(function(name, index) {
+                        return {name: name, value: row[index]};
+                    }).concat({'name': 'rho', 'value': getRhoOnError(test_id, row[0], i)}, {'name': 'waveform', 'value': getWaveform(row) }, {'id': +row[0], 'name': 'status', 'value': is_stable(row[0]) ? 'Stable' : 'Unstable'});
+                })
+        .enter()
+        .append("td")
+        .attr('class', function(d) { return d.name == 'status' ? (is_stable(d.id) ? 'success' : 'danger') : '' })
+        .html(function(d) {
+            return d.name != 'waveform' ? d.value.replace(" 00:00:00.0", "") : populateWaveform.call(this, d); });
 
-        populatePrediction(rows[0]);
-	}, function(data) {
-        //TODO remove failsafe?
-        var lookup = function(id) {
-            row = $('td', $('#patients tbody tr')[ids.indexOf(id)]);
-            return [row[0].innerText, row[1].innerText, row[2].innerText, row[3].innerText]; };
-        data.schema = ['subject_id', 'dob', 'sex'];
-        data.tuples = [lookup(test_id)];
-        data.tuples.push(lookup(ids[+data.tuples[0][0] % ids.length]));
-        data.tuples.push(lookup(ids[+data.tuples[1][0] % ids.length]));
-        data.tuples.push(lookup(ids[+data.tuples[2][0] % ids.length]));
-        data.tuples.push(lookup(ids[+data.tuples[3][0] % ids.length]));
-        data.tuples.push(lookup(ids[+data.tuples[4][0] % ids.length]));
+    $('#similar-patients').fadeIn();
 
-	    var rows = d3.select("#results tbody")
-                     .selectAll("tr")
-	                 .data(data.tuples, function(d) { return d; });
-        rows.enter().append("tr");
-        rows.exit().remove();
-		rows.selectAll("td")
-		    .data(function(row, i) {
-		            return data.schema.map(function(name, index) {
-		                	return {name: name, value: row[index]};
-			            }).concat({'name': 'rho', 'value': map_rho(test_id, row[0], i)}, {'name': 'waveform', 'value': getWaveform(row) }, {'id': +row[0], 'name': 'status', 'value': is_stable(row[0]) ? 'Stable' : 'Unstable'});
-			        })
-		    .enter()
-		    .append("td")
-		    .attr('class', function(d) { return d.name == 'status' ? (is_stable(d.id) ? 'success' : 'danger') : '' })
-	        .html(function(d) {
-                return d.name != 'waveform' ? d.value.replace(" 00:00:00.0", "") : populateWaveform.call(this, d); });
-
-	    $('#similar-patients').fadeIn();
-
-        populatePrediction(rows[0]);
-    });
+    populatePrediction(rows[0]);
 }
 
 function populatePrediction(rows) {
@@ -200,7 +213,7 @@ function plotWaveform(data) {
 
     var line = d3.svg.line()
         .x(function(d,i) { return x(i); })
-        .y(function(d) { return y(d*10); });
+        .y(function(d) { return y(d*3); });
 
     var svg = d3.select(this)
         .append("svg")
@@ -223,7 +236,7 @@ function getWaveform(row) {
     return queries.mapping[queries.mapping.length - 1]['waveform'];
 }
 
-function map_rho(test_id, related_id, index) {
+function getRhoOnError(test_id, related_id, index) {
     return Math.max(0.0, (+related_id == +test_id ? 1 : 0.1) * (1.0 - (+related_id == +test_id ? 0 : index / 10 + 0.1778 + 0.117371 + test_id / 10000 + related_id / 1000000))).toFixed(3);
 }
 
@@ -354,11 +367,12 @@ $(function() {
             .attr('fill', '#000'); });
     }
 
-    function endExecutionAnimation(data, result, index) {
+    function endExecutionAnimation(data, result, index, isError) {
         var context = this;
 
-        setTimeout(function() {
-            duration = result.offset * 0; // +data['elapsedNanos'] / 1E9
+//        setTimeout(function() {
+//            duration = result.offset * 0; // +data['elapsedNanos'] / 1E9
+            var duration = Math.max(+data.elapsedTime / 1000 + result.offset, 5);
 
             window.current_system = undefined;
             result.status = undefined;
@@ -372,10 +386,10 @@ $(function() {
                     d3.select(this).text('Click to Execute')
                       .transition()
                       .attr('fill', '#999'); });
-            setDuration.call(context.parentNode, duration + result.offset);
+            setDuration.call(context.parentNode, duration);
 
             populateResults(data.results);
-        }, result.offset * 1000);
+//        }, result.offset * 1000);
    }
 
   //var margin = {top: 5, right: 60, bottom: 20, left: 220},
@@ -415,29 +429,35 @@ $(function() {
                       var test_id = +$('tr.info td:first').text();
                       var test_index = $('tr.info').parent().children().index($('tr.info'));
                       var query = queries[result.query].replace('@test_id', test_index);
+                      var context = this;
 
                       $.ajax({
-                            url: result.bigdawg_url,
-                            context: this,
-                            method: 'POST',
-                            dataType: 'json',
-                            headers: {
-                              'Accept': 'application/json',
-                              'Content-Type': 'application/json' },
-                            data: JSON.stringify({'query': 'MYRIA(' + query + ')'}),
-                            beforeSend: function(){ startTime = new Date().getTime(); } })
-                          .done(function( data ) {
-                              data.elapsedTime = new Date().getTime() - startTime;
-                              endExecutionAnimation.call(this, data, result, i);
-                          }).error(function(d) {
-                              console.log(d);
-                              var data = { results: [] };
-                              data.elapsedTime = new Date().getTime() - startTime;
-                              console.log(startTime);
-                              console.log(data.elapsedTime);
-                              // TODO: temporary
-                              endExecutionAnimation.call(this, data, result, i);
-                          });
+                       type: 'POST',
+                       url: 'http://localhost:8751/prepare', //TODO move url to config
+                       complete: function() {
+                           $.ajax({
+                                    url: result.bigdawg_url,
+                                    context: this,
+                                    method: 'POST',
+                                    dataType: 'json',
+                                    headers: {
+                                      'Accept': 'application/json',
+                                      'Content-Type': 'application/json' },
+                                    data: JSON.stringify({'query': 'MYRIA(' + query + ')'}),
+                                    beforeSend: function(){ startTime = new Date().getTime(); } })
+                                  .done(function( data ) {
+                                      data.elapsedTime = new Date().getTime() - startTime;
+                                      endExecutionAnimation.call(context, data, result, i, false);
+                                  }).error(function(d) {
+                                      console.log(d);
+                                      var data = { results: [] };
+                                      data.elapsedTime = new Date().getTime() - startTime;
+                                      console.log(data.elapsedTime);
+                                      // TODO: temporary
+                                      endExecutionAnimation.call(context, data, result, i, true);
+                                  });
+                        }
+                      });
                    });
     title.append("text")
         .attr("class", "title")
